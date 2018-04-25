9e8b46bba8d03a76b1b785eb659fdc454386fe7d
yukiiu <664039559@qq.com>
2018-04-25 16:41:21 +0800

    1
    1

---
+++ xue.py
@@ -0,0 +1,8 @@
+﻿jiabin = ['小张','小李','小王']
+buneng = jiabin.pop(0)
+jiabin.insert(0,'小鱼')
+jiabin.reverse()
+print(jiabin)
+print(len(jiabin))
+pingfang = [i**3 for i in range(1,11)]
+print(pingfang[2:])
